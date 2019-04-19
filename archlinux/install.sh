88865370f5bbec6f99aaa66814a7cadd0a7d1bfc
Pedro Gameiro <>
2019-04-20 00:41:20 +0100

    Add: Install pikaur

--- archlinux/install.sh
+++ archlinux/install.sh
@@ -8,9 +8,21 @@ install_pkgs() {
     >&2 echo 'Error: Sorry, archlinux not supported (yet).'
 }
 
+install_pikaur() {
+	cd $(mktemp -d)
+    pacman -S --needed base-devel git
+	git clone 'https://aur.archlinux.org/pikaur.git'
+	cd pikaur
+	makepkg -fsri
+}
+
 main() {
     local a
-    echo 'Install necessary packages[y/n]'; read a
+    echo 'Install pikaur? [y/n]'; read a
+    if [[ "$a" =~ ^(yes|y|Y)$ ]]; then
+        install_pikaur
+    fi
+    echo 'Install necessary packages? [y/n]'; read a
     if [[ "$a" =~ ^(yes|y|Y)$ ]]; then
         install_pkgs
     fi
