282de111ed68d42cc344ecaed26e7c6d90142c86
Pedro Gameiro <>
2019-04-19 14:11:28 +0100

    Fix: Missing part of bt-auto script

---
+++ common/root/opt/dev_scripts/bt-auto-enable-a2dp.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Dependencies:
+# * bluez-tools
+# * expect
+# * perl
+
+function enable_a2dp() {
+	# run connect command in bluetoothctl and wait for resolve of services
+	expect <<< "
+		spawn bluetoothctl
+		send \"connect $mac\r\"
+        log_user 0
+        expect -re \".*Device $mac ServicesResolved: yes\"
+	"
+    
+
+	# enable card in pulseaudio
+	pactl set-card-profile $pulsecard a2dp_sink
+	logger -p info "mac $mac enabled"
+
+	headsetname=`bt-device -l | perl -ne '/(.*) \('$mac'\)/ and print "$1\n"'`
+	notify-send 'Headset connected' "$headsetname" --icon=blueman-headset
+}
+
+function search_headsets() {
+	#sleep 1
+	# in all added devices
+	for mac in `bt-device -l 2>/dev/null | perl -ne '/.*\((.*)\)/ and print "$1\n"'`
+	do
+		# search for connected device with AudioSink service
+		if [[ `bt-device -i $mac 2>/dev/null | perl -00 -ne '/.*Trusted: 1.*\n\s*Blocked: 0.*\n\s*Connected: 1\n\s*UUIDs: .*AudioSink.*/ and print "1\n"'` ]]; then
+
+            # WH-1000XM2
+            if [ "$mac" = '70:26:05:29:55:B2' ]; then
+                logger -p info "found mac: $mac"
+                # convert mac to pulse card name
+                pulsecard=`perl -pe 's/:/_/g' <<< "bluez_card.$mac"`
+                enable_a2dp
+            fi
+		fi
+	done
+	echo "search done"
+}
+
+logger -p info "${BASH_SOURCE[0]}"
+
+# get script owner name
+user=`stat -c %U $0`
+
+if [ "$user" == `whoami` ]; then
+	# if script runned by owner - start main function
+	search_headsets
+elif [ "`w -hs $user`" ]; then
+	# else if user session exist(to prevent running on system startup) - run script from user
+	sudo -u $user ${BASH_SOURCE[0]}
+fi
+
